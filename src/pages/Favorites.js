966bc154c261b3fca71154e72de61c2a5c7a6b9c
João Victor <joao.victor.valentim@hotmail.com>
2023-09-04 14:10:28 -0300

    Add botão de remover favorito

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { motion } from "framer-motion"
+import { motion } from 'framer-motion';
 
 import './Favorites.css';
+
 function Favorites() {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const [searchFavorites, setSearchFavorites] = useState([]);
@@ -17,50 +18,62 @@ function Favorites() {
     setSearchFavorites(storedSearchFavorites);
   }, []);
 
-  return (    
-  <motion.div 
-    className='grid' 
-    animate={{ opacity: 1}}
-    initial={{ opacity: 0}}
-    exit={{opacity: 0}}
-    transition={{
-      duration: 0.8,
-    }}
+  const removeFavorite = (recipeId, type) => {
+    // Remove o favorito do estado e do localStorage
+    if (type === 'general') {
+      const updatedFavorites = favoriteRecipes.filter((recipe) => recipe.id !== recipeId);
+      setFavoriteRecipes(updatedFavorites);
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+    } else if (type === 'search') {
+      const updatedSearchFavorites = searchFavorites.filter((recipe) => recipe.id !== recipeId);
+      setSearchFavorites(updatedSearchFavorites);
+      localStorage.setItem('searchFavorites', JSON.stringify(updatedSearchFavorites));
+    }
+  };
+
+  return (
+    <motion.div
+      className="grid"
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{
+        duration: 0.8,
+      }}
     >
-    <div className="favorites-container">
-      <h2>Meus Favoritos</h2>
+      <div className="favorites-container">
+        <h2>Meus Favoritos</h2>
 
-      <div className="favorite-section">
-        <h3>Favoritos Gerais</h3>
-        <div className="favorite-list">
-          {favoriteRecipes.map((recipe) => (
-            <Link to={'/info/' + recipe.id}>
+        <div className="favorite-section">
+          <h3>Favoritos Gerais</h3>
+          <div className="favorite-list">
+            {favoriteRecipes.map((recipe) => (
               <div className="favorite-item" key={recipe.id}>
                 <img src={recipe.image} alt={recipe.title} />
                 <h3>{recipe.title}</h3>
+                <button onClick={() => removeFavorite(recipe.id, 'general')}>Remover Favorito</button>
                 {/* Adicione outros detalhes da receita, se necessário */}
               </div>
-            </Link>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="favorite-section">
-        <h3>Favoritos de Pesquisa</h3>
-        <div className="favorite-list">
-          {searchFavorites.map((recipe) => (
-            <Link to={'/info/' + recipe.id}>
+        <div className="favorite-section">
+          <h3>Favoritos de Pesquisa</h3>
+          <div className="favorite-list">
+            {searchFavorites.map((recipe) => (
               <div className="favorite-item" key={recipe.id}>
                 <img src={recipe.image} alt={recipe.title} />
                 <h3>{recipe.title}</h3>
+                <button onClick={() => removeFavorite(recipe.id, 'search')}>Remover Favorito</button>
                 {/* Adicione outros detalhes da receita, se necessário */}
               </div>
-            </Link>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-        </motion.div >
+    </motion.div>
   );
 }
+
 export default Favorites;
